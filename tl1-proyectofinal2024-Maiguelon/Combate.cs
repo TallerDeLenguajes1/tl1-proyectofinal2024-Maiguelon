b752c92568eaba5d09276ca78199b0f37a448683
Maiguelon <miguel_carretero@outlook.com>
2024-08-08 18:41:31 -0300

    tocado combate y progrmaa para poder hacer display correcto de la lógica, todavía falta pulir

--- tl1-proyectofinal2024-Maiguelon/Combate.cs
+++ tl1-proyectofinal2024-Maiguelon/Combate.cs
@@ -16,10 +16,10 @@ namespace EspacioCombates
             int ataque = atacante.Caracteristicas.Destreza * atacante.Caracteristicas.Fuerza * atacante.Nivel;
             int efectividad = rand.Next(1, 101); // Valor aleatorio entre 1 y 100
             int defensa = defensor.Caracteristicas.Armadura * defensor.Caracteristicas.Velocidad;
-            int dañoProvocado = (ataque * efectividad - defensa) / 500; // Constante de ajuste
+            int dañoProvocado = (ataque * efectividad - defensa) / 100; // Ajuste para aumentar el daño
 
             // Asegurar que el daño no sea negativo
-            return Math.Max(dañoProvocado, 0);
+            return Math.Max(dañoProvocado, 1);
         }
 
         // Método para realizar un turno de combate
@@ -29,8 +29,8 @@ namespace EspacioCombates
             defensor.Caracteristicas.Salud -= daño;
 
             string fraseAtaque = daño > 10
-                ? Datos.ObtenerFraseAtaque(atacante.Clase, "fuerte")
-                : Datos.ObtenerFraseAtaque(atacante.Clase, "débil");
+                ? Datos.ObtenerFraseAtaque(atacante.Clase, "alto")
+                : Datos.ObtenerFraseAtaque(atacante.Clase, "bajo");
 
             Console.WriteLine($"{atacante.Nombre} ataca a {defensor.Nombre}: {fraseAtaque.Replace("{daño}", daño.ToString())}");
             Console.WriteLine($"{defensor.Nombre} tiene {defensor.Caracteristicas.Salud} puntos de salud restantes.");
@@ -40,13 +40,13 @@ namespace EspacioCombates
         private static void UsarHechizo(Personaje lanzador, Personaje objetivo)
         {
             int nivelMagia = lanzador.Caracteristicas.Magia;
-            if (nivelMagia < 0 || nivelMagia >= ListaHechizos.Hechizos.Length)
+            if (nivelMagia <= 0 || nivelMagia > ListaHechizos.Hechizos.Length)
             {
                 Console.WriteLine($"{lanzador.Nombre} no tiene hechizos disponibles.");
                 return;
             }
 
-            Hechizo hechizo = ListaHechizos.Hechizos[nivelMagia];
+            Hechizo hechizo = ListaHechizos.Hechizos[nivelMagia - 1]; // Ajuste para índices de array
             objetivo.Caracteristicas.Salud -= hechizo.Danio;
 
             string fraseHechizo = Datos.ObtenerFraseHechizo(lanzador.Clase, hechizo.Nombre);
@@ -61,54 +61,54 @@ namespace EspacioCombates
         }
 
         // Método principal para realizar un combate
-       public static void RealizarCombate(Personaje p1, Personaje p2)
-{
-    Console.WriteLine($"El combate entre {p1.Nombre} y {p2.Nombre} ha comenzado!");
-
-    bool p1Turno = true; // Determinar el turno inicial
-
-    // Usar hechizos en el primer turno
-    if (p1.Caracteristicas.Magia > 0)
-    {
-        UsarHechizo(p1, p2);
-        p1Turno = false; // Cambiar turno
-    }
-    else if (p2.Caracteristicas.Magia > 0)
-    {
-        UsarHechizo(p2, p1);
-        p1Turno = true; // Cambiar turno
-    }
-
-    // Continuar el combate hasta que uno de los personajes sea vencido
-    while (p1.Caracteristicas.Salud > 0 && p2.Caracteristicas.Salud > 0)
-    {
-        if (p1Turno)
+        public static void RealizarCombate(Personaje p1, Personaje p2)
         {
-            TurnoCombate(p1, p2);
+            Console.WriteLine($"El combate entre {p1.Nombre} y {p2.Nombre} ha comenzado!");
+
+            bool p1Turno = true; // Determinar el turno inicial
+
+            // Usar hechizos en el primer turno
+            if (p1.Caracteristicas.Magia > 0)
+            {
+                UsarHechizo(p1, p2);
+                p1Turno = false; // Cambiar turno
+            }
+            else if (p2.Caracteristicas.Magia > 0)
+            {
+                UsarHechizo(p2, p1);
+                p1Turno = true; // Cambiar turno
+            }
+
+            // Continuar el combate hasta que uno de los personajes sea vencido
+            while (p1.Caracteristicas.Salud > 0 && p2.Caracteristicas.Salud > 0)
+            {
+                if (p1Turno)
+                {
+                    TurnoCombate(p1, p2);
+                }
+                else
+                {
+                    TurnoCombate(p2, p1);
+                }
+
+                // Cambiar turno
+                p1Turno = !p1Turno;
+            }
+
+            // Determinar el ganador y mostrar el resultado
+            if (p1.Caracteristicas.Salud > 0)
+            {
+                Console.WriteLine($"{p1.Nombre} ha ganado el combate!");
+                p1.SubirNivel();
+            }
+            else
+            {
+                Console.WriteLine($"{p2.Nombre} ha ganado el combate!");
+                p2.SubirNivel();
+            }
+
+            Console.WriteLine("Fin del combate.");
         }
-        else
-        {
-            TurnoCombate(p2, p1);
-        }
-
-        // Cambiar turno
-        p1Turno = !p1Turno;
     }
-
-    // Determinar el ganador y mostrar el resultado
-    if (p1.Caracteristicas.Salud > 0)
-    {
-        Console.WriteLine($"{p1.Nombre} ha ganado el combate!");
-        p1.SubirNivel();
-    }
-    else
-    {
-        Console.WriteLine($"{p2.Nombre} ha ganado el combate!");
-        p2.SubirNivel();
-    }
-
-    Console.WriteLine("Fin del combate.");
 }
 
-    }
-}
