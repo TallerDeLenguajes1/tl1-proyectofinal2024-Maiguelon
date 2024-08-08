5e7588e45231600d34393ae219e9a05e98e9fa9a
Maiguelon <miguel_carretero@outlook.com>
2024-08-08 02:09:51 -0300

    más updates al combate

--- tl1-proyectofinal2024-Maiguelon/Características.cs
+++ tl1-proyectofinal2024-Maiguelon/Características.cs
@@ -4,8 +4,7 @@ namespace EspacioCaracteristica
 {
     public class Caracteristica 
     {
-        // Creo una instancia pública para la aleatoriedad, por comodidad y para evitar que "se repita la semilla"
-        private static readonly Random rand = new(); 
+        private static readonly Random rand = new();
 
         public int Velocidad { get; set; }
         public int Destreza { get; set; }
@@ -14,48 +13,45 @@ namespace EspacioCaracteristica
         public int Salud { get; set; }
         public int Magia { get; set; }
 
-        // Como todos tienen 100 de salud, no es necesario repetir el constructor
         public Caracteristica()
         {
             Salud = 100;
         }
 
-        // Creo un método que asigna propiedades según la clase
         public void GenerarCaracteristicas(string clase)
         {
-            // Solo los magos y los druidas cuentan con magia
             switch (clase)
             {
                 case "Mago":
                     Velocidad = rand.Next(1, 4);
-                    Destreza = rand.Next(1, 3);
+                    Destreza = rand.Next(2, 4);
                     Fuerza = 2;
-                    Armadura = rand.Next(2, 4);
+                    Armadura = rand.Next(3, 5);
                     Magia = rand.Next(5, 8);
                     break;
 
                 case "Guerrero":
-                    Velocidad = rand.Next(4, 7);
-                    Destreza = rand.Next(1, 3);
-                    Fuerza = rand.Next(5, 8);
-                    Armadura = rand.Next(3, 6);
+                    Velocidad = rand.Next(3, 5);
+                    Destreza = rand.Next(2, 4);
+                    Fuerza = rand.Next(4, 5);
+                    Armadura = rand.Next(4, 6);
                     Magia = 0;
                     break;
 
                 case "Picaro":
-                    Velocidad = rand.Next(4, 7);
-                    Destreza = rand.Next(2, 4);
-                    Fuerza = rand.Next(2, 5);
-                    Armadura = rand.Next(1, 3);
+                    Velocidad = rand.Next(4, 6);
+                    Destreza = rand.Next(3, 6);
+                    Fuerza = rand.Next(2, 3);
+                    Armadura = rand.Next(2, 4);
                     Magia = 0;
                     break;
 
                 case "Druida":
-                    Velocidad = rand.Next(4, 7);
-                    Destreza = rand.Next(1, 3);
-                    Fuerza = rand.Next(3, 6);
-                    Armadura = rand.Next(1, 3);
-                    Magia = rand.Next(2, 7);
+                    Velocidad = rand.Next(3, 6);
+                    Destreza = rand.Next(2, 4);
+                    Fuerza = rand.Next(2, 5);
+                    Armadura = rand.Next(2, 4);
+                    Magia = rand.Next(2, 6);
                     break;
             }
         }
--- tl1-proyectofinal2024-Maiguelon/Combate.cs
+++ tl1-proyectofinal2024-Maiguelon/Combate.cs
@@ -6,95 +6,84 @@ namespace EspacioCombate
 {
     public static class Combate
     {
-        private static readonly Random rand = new();
 
-        // Método para iniciar el combate
+        // Método para iniciar un combate
         public static Personaje IniciarCombate(Personaje p1, Personaje p2)
         {
-            Console.WriteLine($"¡Comienza el combate entre {p1.Nombre} y {p2.Nombre}!");
-
-            // Mostrar las características iniciales
-            MostrarEstado(p1);
-            MostrarEstado(p2);
-
-            // Alternar turnos hasta que uno de los personajes sea derrotado
+            Random rand = new();
             bool turnoP1 = true;
+            bool hechizoUsadoP1 = false;
+            bool hechizoUsadoP2 = false;
+
             while (p1.Caracteristicas.Salud > 0 && p2.Caracteristicas.Salud > 0)
             {
                 if (turnoP1)
                 {
-                    RealizarTurno(p1, p2);
+                    if ((p1.Clase == "Mago" || p1.Clase == "Druida") && !hechizoUsadoP1)
+                    {
+                        UsarHechizo(p1, p2);
+                        hechizoUsadoP1 = true;
+                    }
+                    else
+                    {
+                        Atacar(p1, p2);
+                    }
                 }
                 else
                 {
-                    RealizarTurno(p2, p1);
+                    if ((p2.Clase == "Mago" || p2.Clase == "Druida") && !hechizoUsadoP2)
+                    {
+                        UsarHechizo(p2, p1);
+                        hechizoUsadoP2 = true;
+                    }
+                    else
+                    {
+                        Atacar(p2, p1);
+                    }
                 }
 
-                // Mostrar el estado después de cada turno
-                MostrarEstado(p1);
-                MostrarEstado(p2);
+                turnoP1 = !turnoP1; // Cambiar de turno
 
-                turnoP1 = !turnoP1;
+                // Mostrar estado de salud de los personajes
+                Console.WriteLine($"{p1.Nombre}: Salud = {p1.Caracteristicas.Salud}");
+                Console.WriteLine($"{p2.Nombre}: Salud = {p2.Caracteristicas.Salud}");
+                Console.WriteLine();
             }
 
-            // Determinar el ganador y retornar
-            if (p1.Caracteristicas.Salud > 0)
-            {
-                Console.WriteLine($"{p1.Nombre} ha ganado el combate!");
-                return p1;
-            }
-            else
-            {
-                Console.WriteLine($"{p2.Nombre} ha ganado el combate!");
-                return p2;
-            }
+            Personaje ganador = p1.Caracteristicas.Salud > 0 ? p1 : p2;
+            Console.WriteLine($"El ganador del combate es: {ganador.Nombre}");
+
+            // El ganador sube de nivel
+            ganador.SubirNivel();
+            return ganador;
         }
 
-        // Método para realizar un turno de ataque
-        private static void RealizarTurno(Personaje atacante, Personaje defensor)
+        // Método para realizar un ataque
+        private static void Atacar(Personaje atacante, Personaje defensor)
         {
-            // Determinar si se usa un hechizo (primer turno del mago o druida)
-            if (atacante.Clase == "Mago" || atacante.Clase == "Druida")
-            {
-                if (atacante.Nivel == 1)
-                {
-                    UsarHechizo(atacante, defensor);
-                    return;
-                }
-            }
+            Random rand = new();
+            int efectividad = rand.Next(1, 11);
+            int ataque = (atacante.Caracteristicas.Destreza * atacante.Caracteristicas.Fuerza * atacante.Nivel);
+            int defensa = (defensor.Caracteristicas.Armadura * defensor.Caracteristicas.Velocidad);
+            int danio = ((ataque * efectividad) - defensa );
 
-            // Calcular daño normal
-            int efectividad = rand.Next(1, 101);
-            int ataque = atacante.Caracteristicas.Destreza * atacante.Caracteristicas.Fuerza * atacante.Nivel;
-            int defensa = defensor.Caracteristicas.Armadura * defensor.Caracteristicas.Velocidad;
-            int dano = ((ataque * efectividad) / 100) - defensa;
-
-            if (dano > 0)
-            {
-                defensor.Caracteristicas.Salud -= dano;
-                Console.WriteLine($"{atacante.Nombre} ataca a {defensor.Nombre} y causa {dano} puntos de daño.");
-            }
-            else
-            {
-                Console.WriteLine($"{atacante.Nombre} ataca a {defensor.Nombre}, pero la defensa de {defensor.Nombre} lo bloquea.");
-            }
+            // Capear a 50 y agregar esquivada
+            defensor.Caracteristicas.Salud -= Math.Max(danio, 1); // Asegurar que el daño no sea negativo ni cero
+            Console.WriteLine($"{atacante.Nombre} ataca a {defensor.Nombre} y causa {danio} puntos de daño.");
         }
 
         // Método para usar un hechizo
         private static void UsarHechizo(Personaje lanzador, Personaje objetivo)
         {
-            int nivelMagia = lanzador.Caracteristicas.Magia - 1; // índice del hechizo en la lista
-            if (nivelMagia < 0 || nivelMagia >= ListaHechizos.Hechizos.Length) return;
+            int nivelMagia = lanzador.Caracteristicas.Magia;
+            if (nivelMagia < 0 || nivelMagia >= ListaHechizos.Hechizos.Length)
+            {
+                nivelMagia = ListaHechizos.Hechizos.Length - 1;
+            }
 
             Hechizo hechizo = ListaHechizos.Hechizos[nivelMagia];
             objetivo.Caracteristicas.Salud -= hechizo.Danio;
             Console.WriteLine($"{lanzador.Nombre} usa {hechizo.Nombre} y causa {hechizo.Danio} puntos de daño a {objetivo.Nombre}.");
-        }
-
-        // Método para mostrar el estado actual de un personaje
-        private static void MostrarEstado(Personaje p)
-        {
-            Console.WriteLine($"{p.Nombre} - Salud: {p.Caracteristicas.Salud}");
         }
     }
 }
