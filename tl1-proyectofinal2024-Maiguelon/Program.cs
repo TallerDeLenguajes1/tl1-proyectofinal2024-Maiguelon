c25df2db73cf2e2763cd06a86ddfebe9cdd9f437
Maiguelon <miguel_carretero@outlook.com>
2024-08-09 03:49:17 -0300

    Prueba de estética

--- tl1-proyectofinal2024-Maiguelon/Program.cs
+++ tl1-proyectofinal2024-Maiguelon/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using EspacioPersonaje;
 using EspacioCombates;
@@ -15,23 +14,22 @@ namespace ProyectoRPG
         {
             GestionPartida manejadorDePartidas = new GestionPartida();
             EstadoPartida estado = new EstadoPartida();
-            Apuestas gestionApuestas = new Apuestas();
-            double montoTotal = gestionApuestas.CargarMontoTotal();
 
             bool continuar = true;
 
             while (continuar)
             {
                 Console.Clear();
-                Console.WriteLine("Bet for Thrones");
-                Console.WriteLine("1. Nueva Partida");
-                Console.WriteLine("2. Cargar Partida");
-                Console.WriteLine("3. Mostrar Historial de Ganancias");
-                Console.WriteLine("4. Mostrar Historial de Ganadores");
-                Console.WriteLine("5. Salir");
+                ImprimirDecoracion();
+                CentrarTexto("Bienvenido al Torneo de Aventureros!", true);
+                CentrarTexto("1. Nueva Partida", true);
+                CentrarTexto("2. Cargar Partida", true);
+                CentrarTexto("3. Mostrar Historial de Ganadores", true);
+                CentrarTexto("4. Salir", true);
+                ImprimirDecoracion();
                 Console.Write("Seleccione una opción: ");
 
-                string opcion = Console.ReadLine() ?? "";
+                string opcion = Console.ReadLine() ?? "";  // Inicializa la variable opción para asegurarte de que no sea nula
 
                 if (string.IsNullOrWhiteSpace(opcion))
                 {
@@ -44,7 +42,7 @@ namespace ProyectoRPG
                     case "1":
                         estado = await IniciarNuevoTorneo();
                         manejadorDePartidas.GuardarPartida(estado.Participantes, "cuartos", "partida.json", 1, 0);
-                        montoTotal = await JugarTorneo(estado, manejadorDePartidas, gestionApuestas, montoTotal);
+                        await JugarTorneo(estado, manejadorDePartidas);
                         break;
 
                     case "2":
@@ -52,7 +50,7 @@ namespace ProyectoRPG
                         {
                             estado = manejadorDePartidas.CargarPartida("partida.json");
                             Console.WriteLine("Partida cargada exitosamente.");
-                            montoTotal = await JugarTorneo(estado, manejadorDePartidas, gestionApuestas, montoTotal);
+                            await JugarTorneo(estado, manejadorDePartidas);
                         }
                         else
                         {
@@ -60,16 +58,11 @@ namespace ProyectoRPG
                         }
                         break;
 
-
                     case "3":
-                        gestionApuestas.MostrarHistorialGanancias();
-                        break;
-
-                    case "4":
                         MostrarHistorialDeGanadores();
                         break;
 
-                    case "5":
+                    case "4":
                         continuar = false;
                         break;
 
@@ -88,6 +81,19 @@ namespace ProyectoRPG
             Console.WriteLine("Gracias por jugar. ¡Hasta la próxima!");
         }
 
+        static void ImprimirDecoracion()
+        {
+            Console.WriteLine(new string('*', Console.WindowWidth));
+        }
+
+        static void CentrarTexto(string texto, bool saltoLinea = false)
+        {
+            int espacios = (Console.WindowWidth - texto.Length) / 2;
+            Console.Write(new string(' ', espacios) + texto);
+            if (saltoLinea)
+                Console.WriteLine();
+        }
+
         static async Task<EstadoPartida> IniciarNuevoTorneo()
         {
             List<Personaje> participantes = new List<Personaje>();
@@ -96,96 +102,96 @@ namespace ProyectoRPG
             for (int i = 0; i < 8; i++)
             {
                 var personaje = await fabrica.CrearPersonajeAsync();
+                CentrarTexto($"Participante {i + 1}: {personaje.Nombre} {personaje.Epiteto} , {personaje.Edad} años, {personaje.Clase}");
                 participantes.Add(personaje);
-                Console.WriteLine($"Participante {i + 1}: {personaje.Nombre} {personaje.Epiteto} , {personaje.Edad} años, {personaje.Clase}");
             }
 
             return new EstadoPartida { Participantes = participantes, RondaActual = 1 };
         }
 
-        static async Task<double> JugarTorneo(EstadoPartida estado, GestionPartida manejadorDePartidas, Apuestas gestionApuestas, double montoTotal)
-{
-    HistorialJson historialManejador = new HistorialJson();
-    bool salirPorDinero = false;
-
-    while (estado.Participantes.Count > 1 && !salirPorDinero)
-    {
-        if (estado.Participantes.Count == 4 && estado.FaseTorneo != "semifinales")
+        static async Task JugarTorneo(EstadoPartida estado, GestionPartida manejadorDePartidas)
         {
-            estado.FaseTorneo = "semifinales";
-            estado.IndiceCombateActual = 0;
-        }
-        else if (estado.Participantes.Count == 2 && estado.FaseTorneo != "final")
-        {
-            estado.FaseTorneo = "final";
-            estado.IndiceCombateActual = 0;
-        }
+            HistorialJson historialManejador = new HistorialJson();
 
-        Dictionary<string, double> cuotas = gestionApuestas.ObtenerCuotas(estado.Participantes);
-        Console.WriteLine("Vas a la quiniela, las apuestas están:");
-
-        foreach (var cuota in cuotas)
-        {
-            Console.WriteLine($"{cuota.Key}: {cuota.Value}");
-        }
-
-        Console.Write("¿Cuánto desea apostar? ");
-        double montoApostado = double.Parse(Console.ReadLine() ?? "0");
-        Console.Write("¿Por cuál personaje desea apostar? ");
-        string nombrePersonaje = Console.ReadLine() ?? "";
-
-        if (!string.IsNullOrEmpty(nombrePersonaje))
-        {
-            // Busca el personaje en la lista de participantes
-            Personaje personajeApostado = estado.Participantes.FirstOrDefault(p => p.Nombre == nombrePersonaje);
-
-            if (personajeApostado != null)
+            while (estado.Participantes.Count > 1)
             {
-                double ganancia = gestionApuestas.RealizarApuesta(montoApostado, personajeApostado, cuotas);
-                montoTotal += ganancia;
+                if (estado.Participantes.Count == 4 && estado.FaseTorneo != "semifinales")
+                {
+                    estado.FaseTorneo = "semifinales";
+                    estado.IndiceCombateActual = 0; // Reiniciar índice al comenzar una nueva fase
+                }
+                else if (estado.Participantes.Count == 2 && estado.FaseTorneo != "final")
+                {
+                    estado.FaseTorneo = "final";
+                    estado.IndiceCombateActual = 0; // Reiniciar índice al comenzar una nueva fase
+                }
+
+                if (estado.IndiceCombateActual < estado.Participantes.Count / 2)
+                {
+                    estado = await JugarRonda(estado, manejadorDePartidas);
+                }
+                else
+                {
+                    // Si todos los combates en la fase actual ya se completaron, pasa a la siguiente fase
+                    if (estado.Participantes.Count == 4)
+                    {
+                        estado.FaseTorneo = "semifinales";
+                        estado.RondaActual = 2;
+                        estado.IndiceCombateActual = 0;
+                    }
+                    else if (estado.Participantes.Count == 2)
+                    {
+                        estado.FaseTorneo = "final";
+                        estado.RondaActual = 3;
+                        estado.IndiceCombateActual = 0;
+                    }
+                    else
+                    {
+                        // Terminó el torneo
+                        break;
+                    }
+                }
+            }
+
+            if (estado.Participantes.Count == 1)
+            {
+                Console.WriteLine($"El ganador del torneo es {estado.Participantes[0].Nombre}, {estado.Participantes[0].Epiteto}.");
+                historialManejador.GuardarGanador(estado.Participantes[0], "ganadores.json");
+                Console.WriteLine("Partida guardada.");
+                Console.WriteLine("Gracias por jugar. ¡Hasta la próxima!");
+                Environment.Exit(0); // Salir del programa después de la final
+            }
+        }
+
+        static void MostrarHistorialDeGanadores()
+        {
+            HistorialJson historialManejador = new HistorialJson();
+
+            if (historialManejador.Existe("ganadores.json"))
+            {
+                var ganadores = historialManejador.LeerGanadores("ganadores.json");
+                CentrarTexto("Historial de Ganadores:", true);
+                foreach (var ganador in ganadores)
+                {
+                    CentrarTexto($"{ganador.Epiteto} {ganador.Nombre}");
+                }
             }
             else
             {
-                Console.WriteLine("El personaje seleccionado no existe.");
+                CentrarTexto("No se ha encontrado ningún ganador registrado.");
             }
         }
-        else
-        {
-            Console.WriteLine("Debe ingresar un nombre válido para realizar la apuesta.");
-        }
 
-        if (montoTotal <= 0)
-        {
-            Console.WriteLine("Perdiste todo tu dinero, el torneo no te interesa más.");
-            salirPorDinero = true;
-            break;
-        }
-
-        (estado, montoTotal) = await JugarRonda(estado, manejadorDePartidas, gestionApuestas, montoTotal);
-    }
-
-    if (estado.Participantes.Count == 1 && !salirPorDinero)
-    {
-        Console.WriteLine($"El ganador del torneo es {estado.Participantes[0].Nombre}, {estado.Participantes[0].Epiteto}.");
-        historialManejador.GuardarGanador(estado.Participantes[0], "ganadores.json");
-        gestionApuestas.ActualizarHistorialGanancias(montoTotal);
-        Console.WriteLine($"Tu monto total acumulado es: ${montoTotal}");
-        Console.WriteLine("Gracias por jugar. ¡Hasta la próxima!");
-        Environment.Exit(0);
-    }
-    return montoTotal;
-}
-
-
-        static async Task<(EstadoPartida, double)> JugarRonda(EstadoPartida estado, GestionPartida manejadorDePartidas, Apuestas gestionApuestas, double montoTotal)
+        static async Task<EstadoPartida> JugarRonda(EstadoPartida estado, GestionPartida manejadorDePartidas)
         {
             List<Personaje> ganadores = new List<Personaje>();
 
             for (int i = estado.IndiceCombateActual * 2; i < estado.Participantes.Count; i += 2)
             {
-                Console.WriteLine($"{estado.FaseTorneo} - Combate {estado.IndiceCombateActual + 1}: {estado.Participantes[i].Epiteto} vs {estado.Participantes[i + 1].Epiteto}");
+                CentrarTexto($"{estado.FaseTorneo} - Combate {estado.IndiceCombateActual + 1}: {estado.Participantes[i].Epiteto} vs {estado.Participantes[i + 1].Epiteto}");
 
-                await Task.Run(() => Combates.RealizarCombate(estado.Participantes[i], estado.Participantes[i + 1]));
+                // Ejecuta el combate de manera asíncrona
+                await Task.Run(() => RealizarCombateConColor(estado.Participantes[i], estado.Participantes[i + 1]));
 
                 if (estado.Participantes[i].Caracteristicas.Salud > 0)
                 {
@@ -209,7 +215,7 @@ namespace ProyectoRPG
 
                     manejadorDePartidas.GuardarPartida(participantesRestantes, estado.FaseTorneo, "partida.json", estado.RondaActual, estado.IndiceCombateActual);
                     Console.WriteLine("Partida guardada.");
-                    Environment.Exit(0);
+                    Environment.Exit(0); // Salir del programa
                 }
             }
 
@@ -217,7 +223,42 @@ namespace ProyectoRPG
             estado.RondaActual++;
             estado.IndiceCombateActual = 0;
 
-            return (estado, montoTotal);
+            return estado;
+        }
+
+        static void RealizarCombateConColor(Personaje personaje1, Personaje personaje2)
+        {
+            // Determina el color para el personaje1
+            Console.ForegroundColor = ObtenerColorClase(personaje1.Clase);
+            Console.WriteLine($"{personaje1.Nombre} ({personaje1.Epiteto}) ataca a {personaje2.Nombre} ({personaje2.Epiteto})!");
+
+            // Aquí iría la lógica del combate y la descripción de los ataques de personaje1
+            Console.WriteLine($"El {personaje1.Clase} {personaje1.Nombre} lanza un ataque devastador contra {personaje2.Nombre}!");
+
+            // Determina el color para el personaje2
+            Console.ForegroundColor = ObtenerColorClase(personaje2.Clase);
+            Console.WriteLine($"{personaje2.Nombre} ({personaje2.Epiteto}) contraataca!");
+
+            // Aquí iría la lógica del combate y la descripción de los ataques de personaje2
+            Console.WriteLine($"El {personaje2.Clase} {personaje2.Nombre} responde con un golpe certero!");
+
+            // Restablece el color de la consola
+            Console.ResetColor();
+
+            // Simula el combate y determina el ganador
+            Combates.RealizarCombate(personaje1, personaje2);
+        }
+
+        static ConsoleColor ObtenerColorClase(string clase)
+        {
+            return clase.ToLower() switch
+            {
+                "guerrero" => ConsoleColor.Red,
+                "mago" => ConsoleColor.Blue,
+                "picaro" => ConsoleColor.Green,
+                "druida" => ConsoleColor.Yellow,
+                _ => ConsoleColor.White,
+            };
         }
 
         static bool PreguntarSiGuardar()
@@ -225,14 +266,14 @@ namespace ProyectoRPG
             while (true)
             {
                 Console.WriteLine("¿Desea guardar la partida y salir? (s/n)");
-                string respuesta = (Console.ReadLine() ?? "").ToLower();
+                string respuesta = (Console.ReadLine() ?? "").ToLower(); // Asegurar que la entrada no sea nula
                 if (respuesta == "s")
                 {
-                    return true;
+                    return true; // Guardar y salir
                 }
                 else if (respuesta == "n")
                 {
-                    return false;
+                    return false; // Continuar sin guardar
                 }
                 else
                 {
@@ -240,24 +281,6 @@ namespace ProyectoRPG
                 }
             }
         }
-
-        static void MostrarHistorialDeGanadores()
-        {
-            HistorialJson historialManejador = new HistorialJson();
-
-            if (historialManejador.Existe("ganadores.json"))
-            {
-                var ganadores = historialManejador.LeerGanadores("ganadores.json");
-                Console.WriteLine("Historial de Ganadores:");
-                foreach (var ganador in ganadores)
-                {
-                    Console.WriteLine($"{ganador.Epiteto} {ganador.Nombre}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("No se ha encontrado ningún ganador registrado.");
-            }
-        }
     }
 }
+
